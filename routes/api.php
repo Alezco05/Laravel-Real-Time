218a07894f2481053000fcb27a2b079ec9a2dea6
Jhon Calderon <ingdesarrollo1@grupodyp.com>
2020-09-24 14:37:07 -0500

    Mostrando Api en tiempo real

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::apiResource('users', UserController::class);
+
+Route::apiResources([
+    'users' => 'App\Http\Controllers\Api\UserController',
+]);
